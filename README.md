ab65da3e1f24101ae5c3b15cdfbfe7b4492c5900
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-10-01 16:16:52 -0400

    Successfully fetched Poke data
    Created function fetchPokemon to fetch data from the pokemon API

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Project-1
-Beaker project 1
+
+Pokédex Project
+
+A pokedex app directly inspired by the pokedex from Pokemon Ruby/Sapphire and Pokemon Diamond/Pearl.
--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+const allPokemon = [];
+
+const fetchPokemon = (numOfPokemon) => {
+  $.ajax({
+    url: `https://pokeapi.co/api/v2/pokemon/${numOfPokemon}/?limit=151`,
+  }).then(
+    (data) => {
+      console.log(data);
+    },
+    () => {
+      console.log('ERROR');
+    }
+  );
+};
