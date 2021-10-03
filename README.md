aaf9fd43ff08115e6b6e61b76941453fa1bfd2b2
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-10-02 22:26:30 -0400

    Completed html and styling
    Restarted project as a pokemon guessing game. Completed HTML and CSS, working on game functionality

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Pokédex Project
 
 A pokedex app directly inspired by the pokedex from Pokemon Ruby/Sapphire and Pokemon Diamond/Pearl.
+
+Diamond/Pearl reference: https://www.youtube.com/watch?v=cGnIp8pwcE8
+Ruby/Sapphire reference: https://www.youtube.com/watch?v=cnB5ipNMMgU&t=16s
--- app.js
+++ app.js
@@ -1,14 +1,42 @@
-const allPokemon = [];
+class Pokemon {
+  constructor(name, img) {
+    this.name = name;
+    this.img = img;
+  }
+}
 
-const fetchPokemon = (numOfPokemon) => {
-  $.ajax({
-    url: `https://pokeapi.co/api/v2/pokemon/${numOfPokemon}/?limit=151`,
-  }).then(
-    (data) => {
-      console.log(data);
-    },
-    () => {
-      console.log('ERROR');
-    }
-  );
+const fetchPokemon = async (numOfPokemon) => {
+  const pokemon = await $.ajax(
+    `https://pokeapi.co/api/v2/pokemon/${numOfPokemon}`
+  ).then((data) => data);
+
+  return pokemon;
+
+  //   $.ajax({
+  //     url: `https://pokeapi.co/api/v2/pokemon/${numOfPokemon}`,
+  //     type: 'GET',
+  //   }).then(
+  //     (data) => {
+
+  //       return data;
+  //     },
+  //     () => {
+  //       console.log('ERROR');
+  //     }
+  //   );
 };
+
+(async function () {
+  const random151 = Math.floor(Math.random() * 151);
+  const currentPokemon = await fetchPokemon(random151);
+  console.log(currentPokemon.sprites.front_default);
+
+  const poke = new Pokemon(
+    currentPokemon.name,
+    currentPokemon.sprites.front_default
+  );
+
+  $('.pokemon-container').append(
+    $('<img>').attr('src', poke.img).addClass('current-pokemon cover')
+  );
+})();
