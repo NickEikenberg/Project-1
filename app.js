e2fa3ccc7daa18cbe1da120012c7664aab52ade6
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-10-02 23:04:32 -0400

    Updates
    More updates and additional functionality

--- app.js
+++ app.js
@@ -27,16 +27,31 @@ const fetchPokemon = async (numOfPokemon) => {
 };
 
 (async function () {
+  // Fetches a random pokemon from the original 151
   const random151 = Math.floor(Math.random() * 151);
   const currentPokemon = await fetchPokemon(random151);
-  console.log(currentPokemon.sprites.front_default);
 
+  // Saves the current pokemons name and sprite image
   const poke = new Pokemon(
     currentPokemon.name,
     currentPokemon.sprites.front_default
   );
 
-  $('.pokemon-container').append(
-    $('<img>').attr('src', poke.img).addClass('current-pokemon cover')
-  );
+  // Appends the image to the center of the screen
+  const displayPokemon = () => {
+    $('.pokemon-container').append(
+      $('<img>').attr('src', poke.img).addClass('current-pokemon cover')
+    );
+  };
+
+  displayPokemon();
+
+  //   $('.pokemon-container').append(
+  //     $('<img>').attr('src', poke.img).addClass('current-pokemon cover')
+  //   );
+  console.log(poke.name);
+
+  const revealPokemon = (pokemon) => {
+    pokemon.removeClass('cover');
+  };
 })();
